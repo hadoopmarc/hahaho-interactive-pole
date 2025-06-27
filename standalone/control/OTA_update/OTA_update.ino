78eafb0d6b1a12eb9ca087e9fe2cf668fe166c76
HadoopMarc <hadoopmarc@xs4all.nl>
2025-06-27 09:02:17 +0200

    Adds example links

--- standalone/control/OTA_update/OTA_update.ino
+++ standalone/control/OTA_update/OTA_update.ino
@@ -1,5 +1,7 @@
 /*
 Uploading new binaries to the ESP32 over Wi-Fi will speed up the development
+https://randomnerdtutorials.com/esp32-over-the-air-ota-programming/
+https://github.com/arduino-libraries/Arduino_ESP32_OTA
 */
 
 void setup() {
