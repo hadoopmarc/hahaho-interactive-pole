910ccfaf4fe4a36eaffe4cc42419010918235a64
HadoopMarc <hadoopmarc@xs4all.nl>
2025-06-27 10:56:31 +0200

    Updates OTA design plan

--- standalone/control/OTA_update/OTA_update.ino
+++ standalone/control/OTA_update/OTA_update.ino
@@ -1,8 +1,19 @@
 /*
-Uploading new binaries to the ESP32 over Wi-Fi will speed up the development
-https://randomnerdtutorials.com/esp32-over-the-air-ota-programming/
-https://github.com/arduino-libraries/Arduino_ESP32_OTA
-*/
+ * Uploading new binaries to the ESP32 over Wi-Fi will speed up development.
+ * There are examples where the MCU acts as Wi-Fi access point and examples where the
+ * firmware of the MCU is updated in over-the-air (OTA) fashion via a Wi-Fi network
+ * that is shared between the MCU and the programming laptop.
+ * For the Interactive Pole we want to combine these two functionalities. In this way
+ * the system does not require a Wi-Fi network to be present and anyone can login to
+ * the MCU's Wi-Fi network and access the web server for OTA firmware updates.
+ *
+ * The [inspiration source code](../../../inspiration/src/main.cpp) uses an external
+ Wi-Fi network.
+
+ * https://github.com/bovineck/ESP32-OTA-AP/tree/main
+ * https://docs.arduino.cc/libraries/arduinoota/
+ * https://github.com/arduino-libraries/Arduino_ESP32_OTA
+ */
 
 void setup() {
   // put your setup code here, to run once:
